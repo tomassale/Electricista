10eb6453983920d2febea7b0203fccb8ceaaa130
Tomás Sale <tomassale@hotmail.com>
2026-01-31 15:21:41 -0300

    Added Metadata and reduced libraries

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -5,8 +5,37 @@ import { Inter } from 'next/font/google'
 
 export const metadata: Metadata = {
   title: "Electricista Matriculado",
-  description: "Página electricista",
-  keywords: ["Electricista", "Zona Oeste", ""],
+  description: "Electricista Matriculado en Buenos Aires,  Zona Oeste. Urgencias en instalaciones eléctricas de tu vivienda y/o negocio",
+  keywords: [
+      "Electricista",
+      "Electricista matriculado",
+      "Urgencia eléctrica 24hs",
+      "Electricista a domicilio",
+      "Electricista en Zona Oeste",
+      "Electricista en La Matanza",
+      "Electricista en San Justo",
+      "Electricista en Ramos Mejía",
+      "Electricista en Haedo",
+      "Electricista en Morón",
+      "Electricista en Lomas del Mirador",
+      "Puesta a tierra y jabalinas",
+      "Instalación de disyuntores",
+      "Cambio de térmicas",
+      "Cableado y recableado",
+      "Iluminación LED",
+      "Fotocélulas y automáticos",
+      "Porteros eléctricos",
+      "Reparación de cortocircuitos",
+      "Instalación de Aires Acondicionados",
+      "Automatización de portones",
+      "Protectores de tensión",
+      "Mantenimiento eléctrico para consorcios",
+      "Electricidad para locales comerciales",
+      "Medición de consumo",
+      "Presupuesto eléctrico sin cargo",
+      "Electricista de confianza",
+      "Soluciones técnicas para el hogar"
+  ],
   icons: {
     icon: "/favicon.ico"
   }
